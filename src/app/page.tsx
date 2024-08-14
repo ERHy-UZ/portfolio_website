29dd4b5557aa5cd1f594804fe92131d596f1ab6f
ERHy-UZ <jtrejom210900@gmail.com>
2024-08-13 19:25:52 -0600

    Se añadio responsive design al texto y logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       <div className="scanlines z-50"/>
       <main className='opacity-60 blur-[0.8px]'>
         <header className='p-5 w-full'>
-          <p className='uppercase text-5xl font-sf_pixelate font-bold text-cybergreen-200 tracking-widest'>{NAME_}</p>
+          <p className='uppercase text-2xl lg:text-5xl font-sf_pixelate font-bold text-cybergreen-200 tracking-widest'>{NAME_}</p>
           <DefaultComponent/>
         </header>
       </main>
--- src/components/DefaultComponent.tsx
+++ src/components/DefaultComponent.tsx
@@ -18,8 +18,8 @@ export default function DefaultComponent() {
 
     return (
         <section className='flex flex-col space-y-5 h-[54rem] justify-center items-center'>
-            <Image className='cursor-pointer' src={`/Logo_${LOGO_[toggleState]}.svg`} alt='Logo' width={300} height={300} ref={glitch.ref} onClick={handleChangeWords} />
-            <p className='text-cybergreen-100 text-xl font-w95f4 tracking-widest pointer-events-none'>{TITLE_[toggleState]}</p>
+            <Image className='cursor-pointer h-[150px] w-[150px] lg:h-[300px] lg:w-[300px]' src={`/Logo_${LOGO_[toggleState]}.svg`} alt='Logo' width={300} height={300} ref={glitch.ref} onClick={handleChangeWords} />
+            <p className='text-cybergreen-100 lg:text-xl font-w95f4 tracking-widest pointer-events-none'>{TITLE_[toggleState]}</p>
         </section>
     )
 }

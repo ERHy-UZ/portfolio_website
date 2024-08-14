0cb2ca13d82a6cf53e6753403c4e7bce0b170a9b
ERHy-UZ <jtrejom210900@gmail.com>
2024-08-13 23:48:25 -0600

    Pasar parte del CSS a Tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
     <>
-      <div className="scanlines z-50"/>
+      <div className="fixed top-0 left-0 w-full h-full pointer-events-none scanlines z-50"/>
       <main className='opacity-60 blur-[0.8px]'>
         <header className='p-5 w-full'>
           <p className='uppercase text-2xl lg:text-5xl font-sf_pixelate font-bold text-cybergreen-200 tracking-widest'>{NAME_}</p>
--- src/components/DefaultComponent.tsx
+++ src/components/DefaultComponent.tsx
@@ -7,7 +7,7 @@ import { useGlitch, GlitchHandle } from 'react-powerglitch'
 export default function DefaultComponent() {
 
     const LOGO_ = ['Gimikode', 'Gimi']
-    const TITLE_ = ['Gimmicks in Code, Magic in Results', 'Sitio aún no disponible']
+    const TITLE_ = ['Gimmicks in Code, Magic in Results', 'Esta página web no está disponible']
 
     const [toggleState, setToggleState] = useState(0)
     const glitch: GlitchHandle = useGlitch({ playMode: 'always', slice: { hueRotate: false, count: 10 }, glitchTimeSpan: {start: .5, end: .7}})
@@ -18,7 +18,7 @@ export default function DefaultComponent() {
 
     return (
         <section className='flex flex-col space-y-5 h-[54rem] justify-center items-center'>
-            <Image className='cursor-pointer h-[150px] w-[150px] lg:h-[300px] lg:w-[300px]' src={`/Logo_${LOGO_[toggleState]}.svg`} alt='Logo' width={300} height={300} ref={glitch.ref} onClick={handleChangeWords} />
+            <Image className='cursor-pointer h-[150px] w-[150px] lg:h-[300px] lg:w-[300px]' draggable='false' src={`/Logo_${LOGO_[toggleState]}.svg`} alt='Logo' width={300} height={300} ref={glitch.ref} onClick={handleChangeWords} priority/>
             <p className='text-cybergreen-100 lg:text-xl font-w95f4 tracking-widest pointer-events-none'>{TITLE_[toggleState]}</p>
         </section>
     )

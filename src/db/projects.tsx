e540452f7ff1269d28448a012f50a8c19daa6026
ERHy-UZ <jtrejom210900@gmail.com>
2024-08-23 18:41:00 -0600

    imagenes originales

--- src/db/projects.tsx
+++ src/db/projects.tsx
@@ -8,32 +8,32 @@ import type { ProjectType } from "@/types";
 
 export const Projects : ProjectType[] = [
     {
-        image: '/01-export.png',
+        image: '/facturar.jpg',
         title: 'Facturador web',
         descripciones: ['Creación del Front-End para una aplicación web para facturar.', '[PROYECTO DE EMPRESA]'],
         techs: [{ logo: <RiNextjsFill />, nombre: 'Next' }, { logo: <BiLogoJavascript />, nombre: 'JavaScript' }, { logo: <RiTailwindCssFill />, nombre: 'Tailwind' }]
     },
     {
-        image: '/01-export.png',
+        image: '/farmacia.jpg',
         title: 'E-commerce farmacia',
         descripciones: ['Front-End para un E-commerce de una farmacia.', '[PROYECTO DE EMPRESA]'],
         techs: [{ logo: <RiReactjsFill />, nombre: 'React' }, { logo: <BiLogoJavascript />, nombre: 'JavaScript' }, { logo: <FaCss3Alt />, nombre: 'Css' }]
     },
     {
-        image: '/01-export.png',
+        image: '/nutri.jpg',
         title: 'NutriLife',
         descripciones: ['Front-End para aplicación para nutriólogos web y móvil.', '[PROYECTO DE EMPRESA]'],
         techs: [{ logo: <RiNextjsFill />, nombre: 'Next' }, { logo: <RiReactjsFill />, nombre: 'Native' }, { logo: <BiLogoJavascript />, nombre: 'JavaScript' }, { logo: <FaCss3Alt />, nombre: 'Css' }, { logo: <SiMui />, nombre: 'MUI' }]
     },
     {
-        image: '/01-export.png',
+        image: '/propinas.jpg',
         title: 'Calculadora de consumo y propinas',
         descripciones: ['Mini proyecto que te da el precio de lo que consumió y la propina.', 'En este se usaron conceptos como Hooks (useState y useMemo para mejora de performance), así como el uso de un Custom Hook para escalabilidad.'],
         techs: [{ logo: <RiReactjsFill />, nombre: 'React' }, { logo: <BiLogoTypescript />, nombre: 'TypeScript' }, { logo: <RiTailwindCssFill />, nombre: 'Tailwind' }, { logo: <SiMui />, nombre: 'MUI' }],
         link: 'https://calculadora-consumo-propina-gimikode.netlify.app/'
     },
     {
-        image: '/01-export.png',
+        image: '/calorias.jpg',
         title: 'Contador de calorías',
         descripciones: ['Mini proyecto que cuenta las calorías que se queman y consumen.', 'Se puso en práctica los Hooks de React en específico useState, useEffect, useMemo(Para mejora de rendimiento) y useReducer(Para almacenar los datos).'],
         techs: [{ logo: <RiReactjsFill />, nombre: 'React' }, { logo: <BiLogoTypescript />, nombre: 'TypeScript' }, { logo: <RiTailwindCssFill />, nombre: 'Tailwind' }, { logo: <CgFramer />, nombre: 'Framer_Motion' }],

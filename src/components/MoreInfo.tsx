30f8d62a69aa22d7dbe48c409f13cd1496ca28ad
ERHy-UZ <jtrejom210900@gmail.com>
2024-08-27 11:38:46 -0600

    cambio en texto de alt en imagen

--- src/components/MoreInfo.tsx
+++ src/components/MoreInfo.tsx
@@ -11,7 +11,7 @@ export default function MoreInfo() {
           <p className='text-sm lg:text-lg xl:text-2xl font-w95f4 tracking-wider text-cyberwhite mt-2 lg:mt-4 2xl:text-justify'>A lo largo de mi carrera, he trabajado en varios proyectos que han fortalecido mis competencias y mi capacidad de resolver problemas complejos.</p>
         </div>
         <div className='flex justify-center items-center'>
-          <Image className='flex justify-center items-center text-cyberwhite font-w95f4 text-2xl tracking-wider' loading='lazy' src={''} alt='Imagen de José Antonio con su titulo' width={350} height={250} />
+          <Image className='flex justify-center items-center text-cyberwhite font-w95f4 text-sm lg:text-2xl tracking-wider ' loading='lazy' src={''} alt='Imagen de José Antonio con su titulo' width={350} height={250} />
         </div>
       </section>
     </>

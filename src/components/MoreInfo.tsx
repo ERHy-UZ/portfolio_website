f8f988d76c8258102de39fd2862149b062d84d72
ERHy-UZ <jtrejom210900@gmail.com>
2024-08-28 00:30:58 -0600

    se agrego la foto final

--- src/components/MoreInfo.tsx
+++ src/components/MoreInfo.tsx
@@ -10,8 +10,8 @@ export default function MoreInfo() {
           <p className='text-sm lg:text-lg xl:text-2xl font-w95f4 tracking-wider text-cyberwhite 2xl:text-justify'>Actualmente, estoy enfocado en mejorar mis habilidades en tecnologías modernas como <strong className='text-cybergreen-200'>React, Next.js, y React Native</strong>, y también he comenzado a explorar <strong className='text-cybergreen-200 uppercase'>Appian</strong> para desarrollar aplicaciones empresariales. Me gusta crear interfaces de usuario, y me esfuerzo por mantenerme al día con las tendencias y mejores prácticas.</p>
           <p className='text-sm lg:text-lg xl:text-2xl font-w95f4 tracking-wider text-cyberwhite mt-2 lg:mt-4 2xl:text-justify'>A lo largo de mi carrera, he trabajado en varios proyectos que han fortalecido mis competencias y mi capacidad de resolver problemas complejos.</p>
         </div>
-        <div className='flex justify-center items-center'>
-          <Image className='flex justify-center items-center text-cyberwhite font-w95f4 text-sm lg:text-2xl tracking-wider ' loading='lazy' src={''} alt='Imagen de José Antonio con su titulo' width={350} height={250} />
+        <div className='flex justify-center mt-2 lg:mt-4'>
+          <Image className='rounded-md text-cyberwhite font-w95f4 text-sm lg:text-2xl tracking-wider pointer-events-none h-[200px] lg:h-[300px] w-[200px] lg:w-[300px]' loading='lazy' src={'/temp.jpg'} alt='Imagen de José Antonio' width={300} height={300} />
         </div>
       </section>
     </>
